80e0d66db063ca5c867cc4be70c3c337e85f578b
Reapster <reapster100xd@gmail.com>
2026-07-19 20:16:16 +0530

    Update main.py

--- IncrementalProject/main.py
+++ IncrementalProject/main.py
@@ -1,9 +1,9 @@
 from models.product import Product
 from registry.registry import ProductRegistry
 
-laptop = Product("Laptop", 80000, 10)
+laptop = Product("Laptop", 80000, 15)
 mouse = Product("Wireless Mouse", 1500, 50)
-keyboard = Product("Mechanical Keyboard", 5000, 25)
+keyboard = Product("Mechanical Keyboard", 5000, 50)
 
 print("\nProduct Details")
 print("-" * 40)
